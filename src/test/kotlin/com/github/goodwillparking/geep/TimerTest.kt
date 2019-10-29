a1f30034afa50be0bff76839f85f56af09372fee
goodwillparking <48186345+goodwillparking@users.noreply.github.com>
2019-10-29 00:33:01 -0400

    Fix wonky formatting in TimerTest.

--- src/test/kotlin/com/github/goodwillparking/geep/TimerTest.kt
+++ src/test/kotlin/com/github/goodwillparking/geep/TimerTest.kt
@@ -10,28 +10,14 @@ class TimerTest {
 
     @Test
     fun `a state should be able to set a single timer`() = AsyncTestHarness().run {
-        overseer.handleMessage(
-            Stay().async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
+        overseer.handleMessage(Stay().async(SetSingleTimer("t1", Duration.ZERO, "e1")))
         async.fireTimer("t1")
         s1.assertEvents("e1")
     }
 
     @Test
     fun `a state should be able to set a periodic timer`() = AsyncTestHarness().run {
-        overseer.handleMessage(
-            Stay().async(
-                SetPeriodicTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
+        overseer.handleMessage(Stay().async(SetPeriodicTimer("t1", Duration.ZERO, "e1")))
         repeat(10) { i ->
             async.fireTimer("t1")
             val events = generateSequence { "e1" }.take(i + 1).toList()
@@ -41,85 +27,32 @@ class TimerTest {
 
     @Test
     fun `a single timer should not fire if it has been canceled`() = AsyncTestHarness().run {
-        overseer.handleMessage(
-            Stay().async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
-        overseer.handleMessage(
-            Stay().async(
-                CancelTimer(
-                    "t1"
-                )
-            ))
+        overseer.handleMessage(Stay().async(SetSingleTimer("t1", Duration.ZERO, "e1")))
+        overseer.handleMessage(Stay().async(CancelTimer("t1")))
         expectException<CancellationException> { async.fireTimer("t1") }
         s1.assertEvents()
     }
 
     @Test
     fun `a periodic timer should not fire if it has been canceled`() = AsyncTestHarness().run {
-        overseer.handleMessage(
-            Stay().async(
-                SetPeriodicTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
-        overseer.handleMessage(
-            Stay().async(
-                CancelTimer(
-                    "t1"
-                )
-            ))
+        overseer.handleMessage(Stay().async(SetPeriodicTimer("t1", Duration.ZERO, "e1")))
+        overseer.handleMessage(Stay().async(CancelTimer("t1")))
         expectException<CancellationException> { async.fireTimer("t1") }
         s1.assertEvents()
     }
 
     @Test
     fun `a timer should override an existing timer`() = AsyncTestHarness().run {
-        overseer.handleMessage(
-            Stay().async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
-        overseer.handleMessage(
-            Stay().async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e2"
-                )
-            ))
+        overseer.handleMessage(Stay().async(SetSingleTimer("t1", Duration.ZERO, "e1")))
+        overseer.handleMessage(Stay().async(SetSingleTimer("t1", Duration.ZERO, "e2")))
         async.fireTimer("t1")
         s1.assertEvents("e2")
     }
 
     @Test
     fun `a timer should not override an existing timer if it is passively set`() = AsyncTestHarness().run {
-        overseer.handleMessage(
-            Stay().async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
-        overseer.handleMessage(
-            Stay().async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e2",
-                    true
-                )
-            ))
+        overseer.handleMessage(Stay().async(SetSingleTimer("t1", Duration.ZERO, "e1")))
+        overseer.handleMessage(Stay().async(SetSingleTimer("t1", Duration.ZERO, "e2", true)))
         async.fireTimer("t1")
         s1.assertEvents("e1")
     }
@@ -137,14 +70,7 @@ class TimerTest {
             anyObject()
         )).thenThrow(RuntimeException("No timer for you."))
         val s2 = TestState("2")
-        overseer.handleMessage(
-            Start(s2).async(
-                SetSingleTimer(
-                    "t1",
-                    Duration.ZERO,
-                    "e1"
-                )
-            ))
+        overseer.handleMessage(Start(s2).async(SetSingleTimer("t1", Duration.ZERO, "e1")))
         overseer.assertStack(s1, s2)
         s1.assertCounts(1, 0, 1, 1)
         s2.assertCounts(1, 0, 1, 0)
