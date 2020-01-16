e46b554e159c474cdd979faddb66f1eb18160bd8
goodwillparking <48186345+goodwillparking@users.noreply.github.com>
2020-01-15 23:01:34 -0500

    Upgrade from Gradle 4.10 to 6.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,12 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    compile(group = "org.slf4j", name = "slf4j-api", version = "1.7.25")
+    implementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.25")
 
-    testCompile(group = "ch.qos.logback", name = "logback-classic", version = "0.9.26")
-    testCompile(group = "junit", name = "junit", version = "4.12")
-    testCompile(group = "org.hamcrest", name = "hamcrest-all", version = "1.3")
-    testCompile(group = "org.mockito", name = "mockito-all", version = "1.9.5")
+    testImplementation(group = "ch.qos.logback", name = "logback-classic", version = "0.9.26")
+    testImplementation(group = "junit", name = "junit", version = "4.12")
+    testImplementation(group = "org.hamcrest", name = "hamcrest-all", version = "1.3")
+    testImplementation(group = "org.mockito", name = "mockito-all", version = "1.9.5")
 }
 
 configure<JavaPluginConvention> {
